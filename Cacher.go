d0fd4e6a1f8588f431ff1de26b5f05cd5b5a0c34
Chibirev Grigoriy <2000grisha@gmail.com>
2019-11-14 00:32:02 +0300

    Add logging & little refactoring

--- Cacher.go
+++ Cacher.go
@@ -5,7 +5,9 @@ import (
 	"encoding/hex"
 	"flag"
 	"io"
+	"log"
 	"net/http"
+	"os"
 	"runtime"
 	"strconv"
 	"sync"
@@ -74,10 +76,11 @@ func (ri ResponseInfo) AddToResponseMetaHeaders(resw http.ResponseWriter) {
 	resw.Header().Add("Cache-Control", "max-stale="+strconv.FormatUint(uint64(ri.LifeTime/time.Second), 10))
 }
 
-func (ri ResponseInfo) Send(resw http.ResponseWriter, req *http.Request) {
+func (ri ResponseInfo) Send(resw http.ResponseWriter, req *http.Request) (err error) {
 	CopyHeader(ri.Response.Header, resw.Header())
 	ri.AddToResponseMetaHeaders(resw)
-	resw.Write(ri.Body)
+	_, err = resw.Write(ri.Body)
+	return
 }
 
 func (ri ResponseInfo) Update(response *http.Response) {
@@ -89,6 +92,8 @@ func (ri ResponseInfo) Update(response *http.Response) {
 }
 
 type CacheServer struct {
+	config *CacheServerConfig
+
 	upstream        *http.Client
 	upstreamAddress string
 
@@ -97,17 +102,50 @@ type CacheServer struct {
 
 	toSetCache    chan ResponseInfo
 	toRemoveCache chan string
+
+	accessLog         *log.Logger
+	errorLog          *log.Logger
+	cacheLog          *log.Logger
+	upstreamAccessLog *log.Logger
 }
 
-func NewCacheServer(upstreamAddr string) (cs *CacheServer) {
+func NewCacheServer(config *CacheServerConfig) (cs *CacheServer) {
 	cs = new(CacheServer)
 
+	cs.config = config
+
 	cs.upstream = new(http.Client)
-	cs.upstreamAddress = upstreamAddr
+	cs.upstreamAddress = config.UpstreamAddress
 	cs.cache = make(map[string]ResponseInfo)
 
 	cs.toSetCache = make(chan ResponseInfo)
 	cs.toRemoveCache = make(chan string)
+
+	logFlags := log.Lshortfile | log.Ldate | log.Ltime | log.Lmicroseconds | log.LUTC
+	logFileFlags := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile(config.AccessLogPath, logFileFlags, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cs.accessLog = log.New(file, "accessLog: ", logFlags)
+
+	file, err = os.OpenFile(config.ErrorLogPath, logFileFlags, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cs.errorLog = log.New(file, "errorLog: ", logFlags)
+
+	file, err = os.OpenFile(config.CacheLogPath, logFileFlags, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cs.cacheLog = log.New(file, "cacheLog: ", logFlags)
+
+	file, err = os.OpenFile(config.UpstreamAccessLogPath, logFileFlags, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cs.upstreamAccessLog = log.New(file, "upstreamAccessLog: ", logFlags)
 
 	return
 }
@@ -120,6 +158,9 @@ func (cs *CacheServer) ObtainResponse(originalReq *http.Request) (response *http
 
 	proxyReq, err := http.NewRequest(originalReq.Method, proxyURL.String(), originalReq.Body)
 	if err != nil {
+		cs.errorLog.Printf("Error occured during creating request to upstream \"%s\" (original request to \"%s\"): %v\n",
+			proxyURL, originalReq.URL, err,
+		)
 		return
 	}
 
@@ -128,7 +169,16 @@ func (cs *CacheServer) ObtainResponse(originalReq *http.Request) (response *http
 
 	CopyHeader(originalReq.Header, proxyReq.Header)
 
+	cs.upstreamAccessLog.Printf("Sending request \"%s\" to upstream \"%s\"\n", originalReq.URL, proxyURL)
 	response, err = cs.upstream.Do(proxyReq)
+	cs.upstreamAccessLog.Printf("Got response of request \"%s\" to upstream \"%s\": %s %d\n",
+		originalReq.URL, proxyURL, response.Status, response.ContentLength,
+	)
+	if err != nil {
+		cs.errorLog.Printf("Error occured during proxy \"%s\" to upstream \"%s\": %v\n",
+			originalReq.URL, proxyURL, err,
+		)
+	}
 	return
 }
 
@@ -165,11 +215,15 @@ func (cs *CacheServer) Updating(ri ResponseInfo, resw http.ResponseWriter) {
 	}
 }
 
-func (cs *CacheServer) TrySendFromCache(resw http.ResponseWriter, req *http.Request) (ok bool) {
+func (cs *CacheServer) TrySendFromCache(resw http.ResponseWriter, req *http.Request) (ok bool, err error) {
 	path := req.URL.Path
 	if resinfoCache, ok := cs.cache[path]; ok {
 		atomic.AddUint64(&resinfoCache.RequestCount, 1)
-		resinfoCache.Send(resw, req)
+		err = resinfoCache.Send(resw, req)
+		if err != nil {
+			ok = false
+			cs.errorLog.Printf("Sending error occurred during processing \"%s\": %v\n", req.URL, err)
+		}
 	}
 	return
 }
@@ -178,7 +232,14 @@ func (cs *CacheServer) ServeHTTP(resw http.ResponseWriter, req *http.Request) {
 	defer req.Body.Close()
 	path := req.URL.Path
 
-	if !cs.TrySendFromCache(resw, req) {
+	cs.accessLog.Printf("Processing request \"%s\"\n", req.URL)
+
+	if ok, err := cs.TrySendFromCache(resw, req); !ok {
+		if err != nil {
+			resw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
 		waitGroupInterface, ok := cs.processingWaiters.LoadOrStore(path, new(sync.WaitGroup))
 		waitGroup := waitGroupInterface.(*sync.WaitGroup)
 		if !ok {
@@ -186,6 +247,7 @@ func (cs *CacheServer) ServeHTTP(resw http.ResponseWriter, req *http.Request) {
 			resinfo, err := cs.ObtainFirstResponse(req)
 			if err != nil {
 				resw.WriteHeader(http.StatusBadGateway)
+				cs.errorLog.Printf("Gateway error occurred during processing \"%s\": %v\n", req.URL, err)
 				return
 			}
 			cs.toSetCache <- resinfo
@@ -193,7 +255,7 @@ func (cs *CacheServer) ServeHTTP(resw http.ResponseWriter, req *http.Request) {
 			go cs.Updating(resinfo, resw)
 		} else {
 			waitGroup.Wait()
-			if !cs.TrySendFromCache(resw, req) {
+			if ok, _ = cs.TrySendFromCache(resw, req); !ok {
 				resw.WriteHeader(http.StatusInternalServerError)
 				return
 			}
@@ -202,9 +264,12 @@ func (cs *CacheServer) ServeHTTP(resw http.ResponseWriter, req *http.Request) {
 }
 
 func (cs *CacheServer) CacheUpdating() {
+	log.Print("Starting cache updating goroutine\n")
+
 	for true {
 		select {
 		case resinfo := <-cs.toSetCache:
+			cs.cacheLog.Printf("Starting \"%s\" cache entry setting\n", resinfo.Path)
 			cs.cache[resinfo.Path] = resinfo
 			processingWaiterInterface, ok := cs.processingWaiters.Load(resinfo.Path)
 			if ok {
@@ -212,30 +277,77 @@ func (cs *CacheServer) CacheUpdating() {
 				processingWaiter.Done()
 				cs.processingWaiters.Delete(resinfo.Path)
 			}
+			cs.cacheLog.Printf("Set \"%s\" cache entry\n", resinfo.Path)
 		case path := <-cs.toRemoveCache:
+			cs.cacheLog.Printf("Starting \"%s\" cache entry deletion\n", path)
 			delete(cs.cache, path)
+			cs.cacheLog.Printf("Deleted \"%s\" cache entry\n", path)
 		}
 	}
 }
 
-func main() {
+func (cs *CacheServer) ListenAndServe() {
+	log.Print("Starting cache server listening and serving\n")
+
+	http.ListenAndServe(":"+strconv.Itoa(cs.config.ListeningPort), cs)
+}
+
+type CacheServerConfig struct {
+	GoMaxProcs int
+
+	ListeningPort   int
+	UpstreamAddress string
+
+	AccessLogPath         string
+	ErrorLogPath          string
+	CacheLogPath          string
+	UpstreamAccessLogPath string
+}
+
+func NewCacheServerConfig() (conf CacheServerConfig) {
 	goMaxProcs := flag.Int("proc", 1, "Max go procs count")
 	listeningPort := flag.Int("port", 0, "Listening port")
 	upstreamAddress := flag.String("upstream", "", "Upstream address")
+	accessLogPath := flag.String("access-log", "access.log", "Access log file path")
+	errorLogPath := flag.String("error-log", "error.log", "Error log file path")
+	cacheLogPath := flag.String("cache-log", "cache.log", "Cache changes log file path")
+	upstreamAccessLogPath := flag.String("upstream-log", "upstream.log", "Upstream access log file path")
 	flag.Parse()
 
 	if *goMaxProcs == 0 {
 		*goMaxProcs = runtime.NumCPU()
 	}
-	if *listeningPort == 0 {
-		panic("Missing required flag -port")
-	}
-	if *upstreamAddress == "" {
-		panic("Missing required flag -upstream")
-	}
+	conf.GoMaxProcs = *goMaxProcs
 	runtime.GOMAXPROCS(*goMaxProcs)
+	log.Printf("Go max proc is %d\n", *goMaxProcs)
 
-	cacher := NewCacheServer(*upstreamAddress)
+	if *listeningPort == 0 {
+		log.Fatal("Missing required flag -port")
+	}
+	conf.ListeningPort = *listeningPort
+	log.Printf("Server listening port is %d\n", *listeningPort)
+
+	if *upstreamAddress == "" {
+		log.Fatal("Missing required flag -upstream")
+	}
+	conf.UpstreamAddress = *upstreamAddress
+	log.Printf("Upstream server is \"%s\"\n", *upstreamAddress)
+
+	conf.AccessLogPath = *accessLogPath
+	conf.ErrorLogPath = *errorLogPath
+	conf.CacheLogPath = *cacheLogPath
+	conf.UpstreamAccessLogPath = *upstreamAccessLogPath
+
+	return
+}
+
+func main() {
+	log.Print("Initializing\n")
+
+	config := NewCacheServerConfig()
+	cacher := NewCacheServer(&config)
+
 	go cacher.CacheUpdating()
-	http.ListenAndServe(":"+strconv.Itoa(*listeningPort), cacher)
+
+	cacher.ListenAndServe()
 }
